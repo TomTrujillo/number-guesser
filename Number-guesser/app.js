0b6670281542bca00aedd20fdf3e48e81fcbe2ae
Tom_T <tomdtrujillo@gmail.com>
2021-12-29 09:14:05 -0800

    third commit

--- Number-guesser/app.js
+++ Number-guesser/app.js
@@ -1,4 +1,3 @@
-
 /* 
 Game Function:
 1. Player must guess a number between two numbers (min/max)
@@ -11,7 +10,7 @@ Game Function:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Element
@@ -26,6 +25,13 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
+
 // Listen for guess
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(guessInput.value);
@@ -38,17 +44,56 @@ guessBtn.addEventListener('click', function(){
 
 // Check if winning number is chosen
 if(guess === winningNum){
+    // Game over - won
+
+    gameOver(true, `${winningNum} is correct! YOU WIN!!`);
+
+    } else {
+        // Wrong number
+        guessesLeft -= 1;
+
+      if(guessesLeft === 0){
+        // Game over - lost
+
+        gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+      } else {
+        // Game continues - wrong answer
+
+        // Change border color
+        guessInput.style.borderColor = 'red';
+
+        // Clear input
+        guessInput.value = '';
+
+        // Inform player that the wrong number was guessed
+        setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+      }
+    }
+});
+
+
+//Game over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
     // Disable input
     guessInput.disabled = true;
     // Change border color
-    guessInput.style.borderColor = 'green';
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
     // Set message
-    setMessage(`${winningNum} is correct! YOU WIN!!`, 'green');
+    setMessage(msg);
 
-    } else {
+    // Play again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
 
-    }
-});
+// Get Winning Number
+function getRandomNum(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 // Set Message
 function setMessage(msg, color){
